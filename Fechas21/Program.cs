f67c42c82e9f644993ea8a609472aa2966962a4c
JoelCAI <joelargentina12345@gmail.com>
2022-04-08 15:57:48 -0300

    Agregar archivos de proyecto.

---
+++ Fechas21/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fechas21
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Sistema s = new Sistema();
+			s.Iniciar();
+		}
+
+	}
+}
---
+++ Fechas21/Sistema.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fechas21
+{
+    public class Sistema
+    {
+        public void MenuPrincipal()
+        {
+ 
+            Validador.Bienvenida();
+
+            Validador.FechaNow();
+
+            Validador.Despedida();
+        }
+
+        public void Iniciar()
+        {
+            MenuPrincipal();
+        }
+    }
+}
